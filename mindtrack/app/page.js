9883279046bba20eb0da41d1e77d153c0a983ee2
m-bouhaba <maryam.bouhaba@outlook.com>
2026-01-22 15:04:24 +0100

    bismillah

---
+++ mindtrack/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black border text-[#0969f0]">
+      hello
+    </div>
+  );
+}
